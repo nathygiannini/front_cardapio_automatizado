8c1079972267cbf223c8b1dd2cd28d31c4b41cf3
Nathalia Ap Giannini <108088786+nathygiannini@users.noreply.github.com>
2023-11-24 14:35:55 -0300

    24/11 part3

--- componentes/telas/venda/espetinho.js
+++ componentes/telas/venda/espetinho.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, FlatList, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { SearchBar } from 'react-native-elements';
-import logo from '../imagens/logothermasamarela.jpg';
+import logo from '../../imagens/logothermasamarela.jpg';
 
 const espetinho = () => {
   const [quantidade, setQuantidade] = useState(0);
